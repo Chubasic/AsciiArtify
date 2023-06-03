28e194319e35441a01fab65cb2f3ca5f0863a488
chubasic <yoyoeye44@protonmail.com>
2023-06-04 01:51:17 +0300

    rust server, demo gif

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
     let status_line = "HTTP/1.1 200 OK";
-    let contents = "Hello, World!";
+    let contents = "<h1>Hello, World!</h2>";
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
